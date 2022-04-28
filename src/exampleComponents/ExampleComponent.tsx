f4f33e279f98eaf0ed52eeeafa2350504469fce8
jinsangYoo <milkybboy@gmail.com>
2022-04-28 15:35:07 +0900

    PL 전송 테스트 코드 추가 - noUnusedLocals 비활성화

--- src/exampleComponents/ExampleComponent.tsx
+++ src/exampleComponents/ExampleComponent.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 import {getRandomIntInclusive} from './utils'
+import {ACParams, ACS} from '../components'
 
 interface Props {
   text: string
@@ -25,9 +26,40 @@ export const ExampleComponent = ({text}: Props) => {
     }
   }
 
+  const onSendPL = () => {
+    console.log(`clicked onSendPL!!!`)
+
+    const randomValue = getRandomIntInclusive(0, 999).toString()
+    const url = `>>ExampleComponent::sendPL<< >>${randomValue}<<`
+    const params = ACParams.init(ACParams.TYPE.EVENT, url)
+
+    ACS.send(params)
+      .then(response => {
+        console.log('onSendPL::in then!!')
+        if (response) {
+          console.log('response: ' + JSON.stringify(response, null, 2))
+          alert(`success sdk send`)
+        } else {
+          console.log('response is undefined.')
+          alert(`success sdk send but response is undefined`)
+        }
+      })
+      .catch(err => {
+        console.log('onSendPL::in reject!!')
+        if (err) {
+          console.log('err: ' + JSON.stringify(err, null, 2))
+          alert(`fail sdk send`)
+        } else {
+          console.log('err is undefined.')
+          alert(`fail sdk send but err is undefined`)
+        }
+      })
+  }
+
   return (
     <div className={styles.test}>
       Example Component: {text} <br />
+      <button onClick={onSendPL}>send PL</button> <br />
       <button onClick={onSetLocalStorageClick}>onSetLocalStorageClick</button> <br />
       <button onClick={onGetLocalStorageClick}>onGetLocalStorageClick</button>
     </div>
