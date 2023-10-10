94147f8178e624ff1d0f8e257f1f9760bae931bc
jinsang <jinsang@nhndata.com>
2023-10-10 11:03:46 +0900

    RN과 동일하게 index.ts 의 import 문구에서 type 분리

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import {AceConfiguration} from './acone/aceconfiguration'
 import {ACParams} from './acone/acparam'
 import {ACS} from './acone/acs'
-import {
-  ACEResponseToCaller,
-  ControlTowerOfSDK,
-  ACEGender,
-  ACEMaritalStatus,
-} from './common/constant/ACEPublicStaticConfig'
+import type {ACEResponseToCaller, ControlTowerOfSDK} from './common/constant/ACEPublicStaticConfig'
+import {ACEGender, ACEMaritalStatus} from './common/constant/ACEPublicStaticConfig'
 import ACProduct from './acone/acproduct'
 
 export {AceConfiguration, ACParams, ACS, ACEGender, ACEMaritalStatus, ACEResponseToCaller, ControlTowerOfSDK, ACProduct}
