bfdba88d103ce34fb45487a22bde1eafdf012553
jinsang <jinsang@nhndata.com>
2023-11-09 19:09:55 +0900

    APIForOnLoad 구현중

--- src/acone/parameter/APIForOnLoad.ts
+++ src/acone/parameter/APIForOnLoad.ts
@@ -2,7 +2,7 @@ import ACOTask from '../task/ACOTask'
 import {ITaskParams} from '../../common/task/ITaskParams'
 import {ACEResponseToCaller} from '../../common/constant/ACEPublicStaticConfig'
 import ACELog from '../../common/logger/ACELog'
-import {getBrowserName, isSupportNativeSDK} from '../../common/util'
+import {getBrowserName, isEqualSelfWindowAndParentWindow, isSupportNativeSDK} from '../../common/util'
 
 export default class APIForOnLoad extends ACOTask {
   private static _p1TAG = 'APIForOnLoad'
@@ -18,6 +18,14 @@ export default class APIForOnLoad extends ACOTask {
 
     ACELog.d(APIForOnLoad._p1TAG, `isSupportNativeSDK(): ${isSupportNativeSDK()}`)
     ACELog.d(APIForOnLoad._p1TAG, `getBrowserName(): ${getBrowserName()}`)
+    ACELog.d(APIForOnLoad._p1TAG, `isEqualSelfWindowAndParentWindow(): ${isEqualSelfWindowAndParentWindow()}`)
+
+    if (isSupportNativeSDK()) {
+    } else {
+      if (!isEqualSelfWindowAndParentWindow()) {
+        // window.postMessage
+      }
+    }
   }
 
   public didWork(callback: ((error?: object, result?: ACEResponseToCaller) => void) | undefined): void {
@@ -29,6 +37,5 @@ export default class APIForOnLoad extends ACOTask {
   public doneWork(callback: ((error?: object, result?: ACEResponseToCaller) => void) | undefined) {
     super.doneWork(callback)
     ACELog.d(APIForOnLoad._p1TAG, 'doneWork')
-    // const _parameterUtilForOne = ACEParameterUtilForOne.getInstance()
   }
 }
