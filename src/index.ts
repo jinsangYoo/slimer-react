ddba920e4c4ee2eaf9a7c74e93ed9407a3951d9c
jinsang <jinsang@nhndata.com>
2023-11-30 17:51:11 +0900

    코드 가독성 개선을 위해 callback 타입명을 alias

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import {AceConfiguration} from './acone/aceconfiguration'
 import {ACParams} from './acone/acparam'
 import {ACS} from './acone/acs'
-import type {ACEResponseToCaller, ControlTowerOfSDK} from './common/constant/ACEPublicStaticConfig'
+import type {ACSCallback, ACEResponseToCaller, ControlTowerOfSDK} from './common/constant/ACEPublicStaticConfig'
 import {ACEGender, ACEMaritalStatus} from './common/constant/ACEPublicStaticConfig'
 import {ACProduct} from './acone/acproduct'
 import type {ACSForMessage, MessageForIFrame, MessageForResReady} from './common/constant/PostMessage'
 
 export {
+  ACSCallback,
   AceConfiguration,
   ACParams,
   ACS,
